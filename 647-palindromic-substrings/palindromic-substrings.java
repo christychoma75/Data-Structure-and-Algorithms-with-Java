34ea1324c4692facc83fe30c4d15af2570623f3a
Christy Choma <christychoma75@gmail.com>
2025-10-02 22:55:06 -0400

    Time: 6 ms (69.57%) | Memory: 41.5 MB (74.77%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            count += expandAround(s, i, i); //odd length
+            count += expandAround(s, i, i + 1); //even length
+        }
+        return count;
+    }
+    private int expandAround(String s, int left, int right){
+        int count = 0;
+
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+}
